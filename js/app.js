fd7f8a80d410ac1d9fa05d7638faaea1f7dc0627
Clinton Waters <home@MacBook-Pro.localdomain>
2020-06-11 11:19:54 -0400

    google sheet api import - almost there

--- js/app.js
+++ js/app.js
@@ -15,15 +15,10 @@ for (var i = 1; i <= 13; i++) {
 }
 });
 
-// $($gridDiv).each(function(i) {
-//     $(this).addClass('gridItem'+(i+1));
-//    });
-
 $.ajax({
     url: sheetAsJSON
 }).then((data) => {
-    console.log('data', data)
-    let projects = data.feed.entry.map(project => {
+        let projects = data.feed.entry.map(project => {
         return {
             title: project.gsx$title.$t,
             image: project.gsx$image.$t,
@@ -37,15 +32,30 @@ $.ajax({
 
 
 function app(projectsArr) {
-    console.log('inside app - projectsArr', projectsArr)
-    projectsArr.forEach(project => {
-        // //create  an h3
-        // let title = $('<div>').addClass('gridItem')
-        // //assign the title to the value project.title
-        // title.text(project.title)
-        // //append title to the body
-        // $('.gridContainer').append(title)
-    
+    for(let i=0; i < projectsArr.length; i++) {
+        let $gridDiv = $('<div>')
+            .attr('class','gridItem')
+            .css('background-image', `url('${projectsArr[i].image}')`)
+            .css('background-size', 'cover')
+            .css('background-position', 'center center')
+        // let $a = $("<a>")
+        // $a.attr('href', projectsArr[i].url).attr('target', '_blank')
+        // let $hoverDiv = $('<div>').addClass('project-hover')
+        let $projectTitle = $('<div>')
+            .text(projectsArr[i].title)
+            .addClass('project-title');
+        let $projectDescription = $('<div>')
+            .text(projectsArr[i].description)
+            .addClass('project-description')
+        let $button = $('<input>')
+            .attr('type', 'button')
+            .attr('onClick', `parent.open('${projectsArr[i].url}')`)
+            .attr('id', 'project-button')
+            .attr('value', 'View Project')
+        
+        $('.gridContainer').append($gridDiv)
+        $gridDiv.append($projectTitle,$projectDescription, $button)
+        // $gallery.append($a)
+        // $a.append($gridDiv)
         }
-     )
 }
